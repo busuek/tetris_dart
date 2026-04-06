199f36834890868fd21acff2c5ff2e12b5e4cae3
Alexandr Busuek <101875725+busuek@users.noreply.github.com>
2026-04-06 13:48:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/game_screen.dart';
+import 'screens/game_over_screen.dart';
+import 'screens/leaderboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,4 +11,31 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const TetrisApp());
+}
+
+class TetrisApp extends StatelessWidget {
+  const TetrisApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tetris Cloud',
+      theme: ThemeData.dark(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const GameScreen(),
+        '/game': (context) => const GameScreen(),
+        '/leaderboard': (context) => const LeaderboardScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/gameover') {
+          final score = settings.arguments as int;
+          return MaterialPageRoute(
+            builder: (context) => GameOverScreen(score: score),
+          );
+        }
+        return null;
+      },
+    );
+  }
 }
